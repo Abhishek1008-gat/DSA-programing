161921d296f4e727c844d5c47f5f9c114a08b69c
Abhishek1008-gat <ap2683917@gmail.com>
2025-10-16 14:46:39 +0530

    Add files via upload

---
+++ gcd  of  two numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int gcd (int a,int b)
+{
+    if(b==0){
+            return a;
+    }
+    else{
+        return gcd(b, a%b);}
+    }
+int main(){
+int a,b;
+printf("\n read values for a and b: ");
+scanf("%d %d",&a,&b);
+printf("\n gcd %d & %d is %d \n",a,b,gcd(a,b));
+return 0;
+}
+
